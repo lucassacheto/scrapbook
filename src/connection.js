775d0940e16337b45ea919aaa75340e7dd199a53
Lucas Sacheto <lcsacheto@gmail.com>
2023-11-20 22:32:46 -0500

    .env variables added

--- src/connection.js
+++ src/connection.js
@@ -1,14 +1,21 @@
 import {initializeApp} from 'firebase/app'
 import {getFirestore, collection} from 'firebase/firestore'
 
+const apiKey = import.meta.env.VITE_API_KEY;
+const authDomain = import.meta.env.VITE_AUTH_DOMAIN;
+const projectId = import.meta.env.VITE_PROJECT_ID;
+const storageBucket = import.meta.env.VITE_STORAGE_BUCKET;
+const messagingSenderId = import.meta.env.VITE_MESSAGING_SENDER_ID;
+const appId = import.meta.env.VITE_APP_ID;
+
 export const firebaseConnection = (userLocalStorage) => {
     const firebaseConfig = {
-        apiKey: "AIzaSyAU1TM-L3RbkMRThHg2flKYCw8fNhV7apM",
-        authDomain: "scrapbook-f460c.firebaseapp.com",
-        projectId: "scrapbook-f460c",
-        storageBucket: "scrapbook-f460c.appspot.com",
-        messagingSenderId: "1031015130332",
-        appId: "1:1031015130332:web:91275fc6fb401445578019"
+        apiKey: apiKey,
+        authDomain: authDomain,
+        projectId: projectId,
+        storageBucket: storageBucket,
+        messagingSenderId: messagingSenderId,
+        appId: appId
     };
     
     //FIREBASE  init firebase aop
